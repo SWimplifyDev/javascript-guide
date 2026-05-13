5dda823ded7c0364a1bad344bda80575b78c6a40
SWimplifyDev <info.swimplify@gmail.com>
2026-05-13 15:36:01 +0930

    Update variables.js

--- fundamentals/variables.js
+++ fundamentals/variables.js
@@ -1,2 +1,18 @@
+/**
+ * ======================================
+ * TOPIC: map()
+ * ======================================
+ *
+ * PURPOSE:
+ * Transforms each item into a new value.
+ *
+ * RETURNS:
+ * A new array.
+ *
+ * IMPORTANT:
+ * Does not mutate original array.
+ */
+
+console.log(doubled);
 var time = "3 PM";
 console.log(time);
